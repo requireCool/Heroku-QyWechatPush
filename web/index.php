ae535fd4b98e7fc54602026771e61bccaeb67ef4
requireCool <1019715750@qq.com>
2021-02-28 12:02:39 +0800

    web

--- web/index.php
+++ web/index.php
@@ -1,25 +1,57 @@
 <?php
+//判断变量是否存在
+if (isset($_GET['type'])) {
+    $file = $_GET['type'];
+    include('./' . $file . '.php');
+}
 
-require('../vendor/autoload.php');
+//此处配置你的信息
+$corpid = "";
+$secret = "";
+$agentid = 1000002;
 
-$app = new Silex\Application();
-$app['debug'] = true;
+//获取access_token
+$url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' . $corpid . '&corpsecret=' . $secret;
+$token = https_request($url);
+$access_token = $token['access_token'];
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+//配置消息通知参数
+if (isset($_GET['msg'])) {
+    $content = $_GET['msg'];
+}
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+// 文本格式
+$data = [
+    'touser' => '@all',
+    'msgtype' => 'text',
+    'agentid' => $agentid,
+    'text' => [
+        'content' => $content
+    ]
+];
+$messageUrl = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' . $access_token;
+$message = https_request($messageUrl, 'post', $data);
+echo json_encode($message);
 
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+// CURL
+function https_request($url, $format = 'get', $data = null)
+{
+    $headerArray = array("Content-type:application/json;", "Accept:application/json");
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+    if ($format == 'post') {
+        curl_setopt($curl, CURLOPT_POST, 1);
+        if ($data) {
+            $data  = json_encode($data);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+    }
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArray);
+    $data = json_decode(curl_exec($curl), true);
+    // $data=curl_exec($curl);
+    curl_close($curl);
+    return $data;
+}
---
+++ web/weather.php
@@ -0,0 +1,53 @@
+<?php
+require './Querylist/phpQuery.php';
+require './Querylist/QueryList.php';
+
+use QL\QueryList;
+
+if ($_GET['loc']) {
+    $location = $_GET['loc'];
+} else {
+    $location = 'tianhequ'; //默认显示区域
+}
+
+$url = 'http://m.tianqi.com/' . $location;
+
+
+$rules = [
+    // 位置
+    'location' => ['.hhx_index_newHead_l>text', 'text'],
+    // 更新时间
+    'date' => ['#nowHour', 'text'],
+    // 温度
+    'temp' => ['.now', 'text'],
+    // 湿度
+    'shidu' => ['.b2', 'text'],
+    // 今日温度
+    'today' => ['.temp>.txt', 'text'],
+    // 风向
+    'wind' => ['.b3', 'text'],
+    // 空气质量
+    'kongqi' => ['.b1', 'text'],
+];
+
+$data = QueryList::Query($url, $rules,'', 'utf-8', 'utf-8')->data;
+// echo "<pre>";
+// print_r($data);
+// die();
+$data  = $data[0];
+// $data['date'] = date("Y-m-d");
+
+//排版文字
+$content = $data['location'] . " - " . $data['temp'] . "\n" . " " . "\n更新时间 " . $data['date'] . "\n" . " " . "\n" . $data['today'] . "\n" . " " . "\n" . $data['shidu'] . "\n" . " " . "\n" . $data['wind'] . "\n" . " " . "\n空气质量" . $data['kongqi'];
+
+
+
+function geturldata($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    $content = curl_exec($ch);
+    return $content;
+}
